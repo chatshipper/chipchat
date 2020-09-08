624795bd8fdbf34bcc34a3b76669b1ba0de03448
Mischa Molhoek <mischamolhoek@gmail.com>
2020-09-08 16:24:07 +0200

    only pagination.each as active example

--- examples/pagination.js
+++ examples/pagination.js
@@ -2,18 +2,19 @@ const Sdk = require('../lib/chipchat');
 
 const sdk = new Sdk();
 // examples of pagination.all
+
+/*
+
 (async () => {
     const messages = await sdk.messages.list({ conversation: '5e063af76ced54001123a503',
-        pagination: {
-            limit: 5,
-            countLimit: 20,
-            backoff: 1000
-        }
+        pagination: { }
     });
     console.log('messages: ', messages.length, messages.map(m => m.id));
 })();
 
+// or
 sdk.messages.list({ conversation: '5e063af76ced54001123a503', pagination: { limit: 5, countLimit: 20, backoff: 1000 } }).then(messages => { console.log('nr of messages:', messages.length, messages.map(m => m.id)); });
+*/
 
 // examples of pagination.each
 (async () => {
