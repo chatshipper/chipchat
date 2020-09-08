c7d17b152a2a5bdaea50a327fe6614a292aa8849
Mischa Molhoek <mischamolhoek@gmail.com>
2020-09-08 18:09:22 +0200

    adjusted pagination example

--- examples/pagination.js
+++ examples/pagination.js
@@ -11,23 +11,36 @@ const sdk = new Sdk();
     });
     console.log('messages: ', messages.length, messages.map(m => m.id));
 })();
+*/
 
-// or
-sdk.messages.list({ conversation: '5e063af76ced54001123a503', pagination: { limit: 5, countLimit: 20, backoff: 1000 } }).then(messages => { console.log('nr of messages:', messages.length, messages.map(m => m.id)); });
+/*
+sdk.messages.list({
+    conversation: '5e063af76ced54001123a503',
+    pagination:
+    {
+        limit: 5,
+        countLimit: 20,
+        backoff: 1000
+    }
+}).then(messages => { console.log('nr of messages:', messages.length, messages.map(m => m.id)); });
 */
 
 // examples of pagination.each
 (async () => {
-    const pagination = sdk.messages.list({ conversation: '5e063af76ced54001123a503',
-        pagination: {
-            iterate: true,
-            limit: 5,
-            countLimit: 20,
-            backoff: 1000
+    try {
+        const pagination = sdk.messages.list({
+            pagination: {
+                iterate: true,
+                limit: 5,
+                countLimit: 20,
+                backoff: 1000
+            }
+        });
+        //eslint-disable-next-line
+        for await (const message of pagination) {
+            console.log(message.id);
         }
-    });
-    //eslint-disable-next-line
-    for await (const message of pagination) {
-        console.log(message.id);
+    } catch (e) {
+        console.log(e);
     }
 })();
