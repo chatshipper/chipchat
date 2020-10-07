16e54ae46472101a9bfe8ef1f90205eef525b086
Mischa Molhoek <mischamolhoek@gmail.com>
2020-10-07 19:10:03 +0200

    example change

--- examples/mixins/tokens-in-google-secrets-store/example-usage.js
+++ examples/mixins/tokens-in-google-secrets-store/example-usage.js
@@ -1,8 +1,9 @@
 /*
- * before you can use this, you have to set the 5f773ebbe86b2e001d9cba47_token and
- * 5f773ebbe86b2e001d9cba47_refreshToken in the store from the CLI
- * with echo -n "<paste your token here>" || gcloud secrets create 5f773ebbe86b2e001d9cba47_token
- * --data-file=- --replication-policy automatic --project=<paste your project here>
+ * before you can use this, you have to set the <botid>_token
+ * in the store from the CLI with
+ * echo -n '{"token":"<paste your token here>", "refreshToken":"<paste your refreshToken here>"}' \
+ * | gcloud secrets create $BOTID_token
+ * --data-file=- --replication-policy automatic --project $GOOGLEPROJECT
  */
 
 const ChipChat = require('chipchat');
