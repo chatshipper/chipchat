87b129124ada501bb4fcfdb0b5f71cd6088e9013
Mischa Molhoek <mischamolhoek@gmail.com>
2020-10-15 12:15:05 +0200

    copy forms example

---
+++ examples/copyformsbetweensorgs.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Bot = require('../lib/chipchat');
+
+const log = console.log;
+const api = new Bot({
+    host: 'https://development-api.chatshipper.com',
+    token: process.env.TOKEN
+});
+
+const logErr = err => err && log('error', err.toString());
+
+// let us know when something ges wrong in api
+api.on('error', logErr);
+
+const fromOrgId = process.env.ORG1;
+const toOrgId = process.env.ORG2;
+
+log(`copying forms from ${fromOrgId} to ${toOrgId}`);
+api.forms.list({ organization: fromOrgId }).then(async (forms) => {
+    if (forms) {
+        forms.forEach(async (form) => {
+            log(`copying form ${form.id}`, form);
+            await api.forms.create({ ...form, organization: toOrgId }).catch(logErr);
+        });
+    } else {
+        log('no forms found');
+    }
+});
